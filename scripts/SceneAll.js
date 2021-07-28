a78f9cce1fed351ec28f753991444f1be48b0749
Chris Kabat <chkabat@microsoft.com>
2021-07-28 08:00:44 -0500

    Last

--- scripts/SceneAll.js
+++ scripts/SceneAll.js
@@ -3,7 +3,7 @@ function populateSceneAllText(){
     document.getElementById('subtitle').innerHTML = `
     Branko Milanovic and Christopher Lakner did a study in which they collected real incomes
     across the globe from approximately 120 countries.  The units are 2005 PPP which corresponds
-    approximately to US dollars in the year 2005. There study showed there has been an absence
+    approximately to US dollars in the year 2005. Their study showed there has been an absence
     of real income growth for certain parts of the population.  This narrative visualization gives a few
     views of that analysis using their World Panel Income Distribution 2013 (LM-WPID) dataset.
     `;
